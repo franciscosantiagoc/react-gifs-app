87f616e02bbc2e27fa8f79b1a6788a54b4433ace
Francisco Santiago <fcosantiagoc@gmail.com>
2025-03-09 19:51:04 -0600

    Agregando sistema de favoritos

--- src/Components/CategoryCard.jsx
+++ src/Components/CategoryCard.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaRegHeart, FaHeart } from "react-icons/fa6";
+import { addFavoriteGif, isAFavoriteGif } from "../utils/favoritesFunctions";
 
-export const CategoryCard = ({ title, tinyImg, username, largeImg }) => {
+export const CategoryCard = (image) => {
+  const { id, title, tinyImg, username, largeImg, notify } = image;
+  const hasSavedInFavorite = isAFavoriteGif(image);
+  const [isFavorite, setIsFavorite] = useState(hasSavedInFavorite);
+  
+  
+  const handlerClick = () => {
+    let isSaved = addFavoriteGif(image);
+    setIsFavorite(isSaved);
+    notify(`${isSaved ? 'Se ha guardado a favoritos.' : 'Se ha eliminado de favoritos.'}`)
+  };
+
   return (
     <div className="card category-card">
-        <div className="card-title">{title ? title : username}</div>
-        <div className="card-image">
-            <img src={tinyImg.url} alt={title} />
-        </div>
-        <div className="card-actions">
-            
-        </div>
+      <div className="card-title">{title ? title : username}</div>
+      <div className="card-image">
+        <img src={tinyImg.url} alt={title} />
+        <span className="icon-content" onClick={handlerClick}>
+          {isFavorite ? <FaHeart /> : <FaRegHeart />}
+        </span>
+      </div>
+      <div className="card-actions"></div>
     </div>
-  )
-}
+  );
+};
--- src/Components/CategoryListGrid.jsx
+++ src/Components/CategoryListGrid.jsx
@@ -2,9 +2,14 @@ import { useEffect, useState } from "react";
 import './../assets/categoryListGrid.css';
 import { CategoryCard } from "./CategoryCard";
 import { getImages } from "../Services/GifServices";
+import { ToastContainer, toast } from 'react-toastify';
 export const CategoryListGrid = ({ category }) => {
   
   const [images, setimages] = useState([]);
+
+  const notify = (message) => {
+    toast.success(message)
+  }
 
   const fetchCategoryImages = async () => {
     let imagesResult = await getImages(category);
@@ -23,7 +28,7 @@ export const CategoryListGrid = ({ category }) => {
                 <div className="category-grid">
                     {
                         images.map((image) => 
-                            <CategoryCard key={image.id} {...image}/>
+                            <CategoryCard key={image.id} {...image} notify={notify}/>
                         )
                     }
                 </div>
@@ -32,6 +37,7 @@ export const CategoryListGrid = ({ category }) => {
         {
             images.length === 0 && (<p className="text-muted">No se han encontrado resultados.</p>)
         }
+        <ToastContainer />
     </div>
     );
 };
