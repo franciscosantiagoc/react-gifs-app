c0748723844193a74e822b25c51e9aeeaa2ba825
Francisco Santiago <fcosantiagoc@gmail.com>
2025-04-04 23:34:00 -0600

    Agregando propTypes a componente category search

--- src/Components/CategorySearch.jsx
+++ src/Components/CategorySearch.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import React, { useState } from 'react'
 
 export const CategorySearch = ({
@@ -26,4 +27,8 @@ export const CategorySearch = ({
         </div>
     </form>
   )
+}
+
+CategorySearch.propTypes = {
+    handleSearch: PropTypes.func.isRequired
 }
